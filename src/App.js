f4b90322db8bf34407bd9dc636cf99847b81bdcd
Harvey Francois <har.francois@gmail.com>
2022-02-23 22:08:52 -0800

    I think I broke it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image'
+import Cards from './Cards.js';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class App extends React.Component {
     this.state = {
       city: '',
       cityData: {},
-      cityMap: {}
+      weatherData: {}
     };
   }
   handleInput = (e) => {
@@ -20,43 +21,50 @@ class App extends React.Component {
       city: e.target.value
     })
   };
-  handleSubmit = async (e) => {
+  displayCityData = async (e) => {
     e.preventDefault();
     let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-    
-    
-
-
     console.log(cityData.data[0]);
+    this.displayWeatherData();
+    this.setState({
+      cityData: cityData.data[0],
+      displayCityData: true
+
+    });
   };
-  getMap = async (e) => {
-    e.preventDefault();
-    let cityMap = await axios.get(`https://maps.locationiq.com/v3/staticmap&key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityData.data[0].lat},${cityData.data[0].lon}&zoom=10`)
-  }
+
+  displayWeatherData = async (e) => {
+    
+    let weatherData = await axios.get(`${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.city}`);
+    console.log(weatherData.data);
+    this.setState({ cityData: weatherData });
+  };
+
 
   render() {
-    // let cityName = this.city
 
     return (
+
       <>
-        <h1>{cityData.data[0].name}</h1>
+
         <Form
           onSubmit={this.handleSubmit} >
-          
-
           <Form.Label>Search For A City</Form.Label>
           <Form.Control
             type="text"
             placeholder="City Name"
             onInput={this.handleInput}
           />
+
           <Button
             variant="primary"
             type="submit">
             Explore
           </Button>
         </Form>
-        <Image>{this.getMap}</Image>
+
+
+
       </>
     );
   }
