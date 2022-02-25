b5334d3763e1a92e5806dc41fd3e1cd71ade7d6f
Harvey Francois <har.francois@gmail.com>
2022-02-24 23:03:11 -0800

    map is rendering and information progress being made

---
+++ src/MovieData.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+class MovieData extends React.Component {
+  render() {
+    return (
+      <>
+        <Card>
+          <Card.Body>
+            <Card.Text>{}</Card.Text>
+            <Card.Text>{}</Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    )
+  }
+};
+
+export default MovieData; 
